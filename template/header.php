e713e4db11eaca68605768349607e5d1e8d61810
Noor M. I. Albardawil <67733645+noor2020-eng@users.noreply.github.com>
2020-07-14 11:13:30 +0300

    first commit

---
+++ template/header.php
@@ -0,0 +1,53 @@
+<?php
+  session_start();
+//$_SESSION['name'] = 'yoshi';
+  //to delete a name.
+  if ($_SERVER['QUERY_STRING'] == 'noname') {
+    //unset($_SESSION['name']);
+    session_unset();
+  }
+
+  $name = $_SESSION['name'] ?? 'Guest'; //if name exists then its the name else its Guest.
+
+  //get cookie
+  $gender = $_COOKIE['gender'] ?? 'unknown' ;
+
+ ?>
+
+<head>
+  <title>Noor Pizza</title>
+  <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <style type="text/css">
+    .brand{
+      background: #cbb09c !important;
+    }
+    .brand-text{
+      color:#cbb09c !important;
+    }
+    form{
+      max-width: 460px;
+      margin: 20px auto;
+      padding: 20px;
+    }
+    .pizza{
+      width: 100px;
+      margin: 20px auto;
+      padding: 20px;
+      display: block;;
+      position: relative;
+      top: -30px;
+    }
+    </style>
+</head>
+<body class="grey lighten-4">
+  <nav class="white z-depth-0">
+    <div class="container">
+      <a href="index.php" class="brand-logo brand-text">Noor Pizza</a>
+      <ul id="nav-mobile" class="right hide-on-small-and-down">
+        <li class="grey-text">Hello <?php echo htmlspecialchars($name); ?></li>
+        <li class="grey-text">( <?php echo htmlspecialchars($gender); ?>)</li>
+        <li> <a href="add.php" class="btn brand z-depth-0">Add a Pizaa</a> </li>
+      </ul>
+    </div>
+  </nav>
